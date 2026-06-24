25934dd8bc89be70e6a3a7e08c16cb1b55f2eddc
suil0304 <eagle030410@gmail.com>
2026-06-24 16:15:11 +0900

    #2.10

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 [노마드코더와 함께하는 JavaScript 개발](https://nomadcoders.co/javascript-for-beginners)
 -----------------------------------------
 JavaScript로 크롬 앱을 만들며 배워봅시다. <br>
+<small><a href="https://momentumdash.com/">Momentum</a>이라는 앱을 클론코딩하는 과정입니다.</small> <br>
 <small>JSDoc은 강의에서 나오지 않는 내용으로, JavaScript의 타입 추론을 TypeScript처럼 잡기 위하여 사용하였습니다.</small>
---
+++ js/app.js
@@ -0,0 +1,38 @@
+const caculator = {
+    /**
+     * 둘을 더해보는 어드밴쳐 타임
+     * @param {number} a 
+     * @param {number} b 
+     * @returns {number}
+     */
+    add: function(a, b) {
+        return a + b;
+    },
+    /**
+     * 둘을 빼보는 어드밴쳐 타임
+     * @param {number} a 
+     * @param {number} b 
+     * @returns {number}
+     */
+    sub: function(a, b) {
+        return a - b;
+    },
+    /**
+     * 둘을 곱해보는 어드밴쳐 타임
+     * @param {number} a 
+     * @param {number} b 
+     * @returns {number}
+     */
+    mult: function(a, b) {
+        return a * b;
+    },
+    /**
+     * 둘을 나눠보는 어드밴쳐 타임
+     * @param {number} a 
+     * @param {number} b 
+     * @returns {number}
+     */
+    div: function(a, b) {
+        return a / b;
+    }
+};
