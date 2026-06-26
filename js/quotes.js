b55e6ed7ee29e9e71a330d98dab25b8ade0b2224
suil0304 <eagle030410@gmail.com>
2026-06-26 20:27:45 +0900

    #6.0, 명?언록 작성 중

---
+++ js/quotes.js
@@ -0,0 +1,49 @@
+/**
+ * @typedef {{
+ *      quote: string;
+ *      author: string;
+ *      from?: string;
+ * }} Quote
+ */
+
+/**
+ * 명?언집
+ * @type {Quote[]}
+ */
+const quotes = [
+    {
+        quote: "흐윽... 흐으...!",
+        author: "디아나",
+        from: "시즌 2 3챕터 에피소드 15 중"
+    },
+    {
+        quote: "진짜 시리어스한 건가요? 골치 아프고 몇 개월 동안 질질 끄는 그런 건가요?",
+        author: "오팔",
+        from: "확률 측정: 선악의 퍼센테이지! 중"
+    },
+    {
+        quote: "슈로는... 아니, 우로스는... 모두가 바라는 영웅이 되기 위해서 얼마나 큰 노력을 했을까?",
+        author: "교주",
+        from: "그을음 끝에 피어나는 빛무리 중"
+    },
+    {
+        quote: "은행을 털자. 5분이면 1억 '엔'을 벌 수 있어.",
+        author: "스나오오카미 시로코",
+        from: "Vol.1 대책위원회 편 제 1장, 대책위원회의 기묘한 하루 중"
+    },
+    {
+        quote: "*crys* why...",
+        author: "Corrupted Matt",
+        from: "FNF CORRUPTION MOD ' EVIL BF SAVES CORRUPTION MATT !! SAVING DAY 1 중"
+    },
+    {
+        quote: "Beep?",
+        author: "Boyfriend",
+        from: "모든 때"
+    },
+    {
+        quote: "흐윽⋯! 더는 못 참아!",
+        author: "에슈르",
+        from: "멜트다운 버터 중"
+    }
+]
